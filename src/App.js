d5b546735f60087b77749d6af09c79d76bac969b
Vishmitha-Kumar <vishmithak05@gmail.com>
2024-02-09 20:17:29 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Page from './Components/Page'
+import './assets/css/page.css'
+function App(){
+  return(
+       <Page/>
+  )
+  }
+  export default App
---
+++ src/Components/Page.js
@@ -0,0 +1,99 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <>
+    <section>
+        <div className='layout'> 
+            <div className='content1'>
+                <div className='cont1'>
+                
+                   <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxT0TGuuOhC9RUHAHE6Krw9vMOtQSUryeLFhspIEGihg&s"/>
+                
+                <div className='details'>
+                <input type="submit" value="View details" className="auth-btn" />
+                </div>
+                </div>
+                <div className='container'>
+                <h2>AVANTA FEST</h2>
+                <h4>Mode:Offline/Online</h4>
+                <h4>Sri Krishna College of Technology,Coimbatore</h4>
+                <h5>Start Date:1 March 2024</h5>
+                <h5>End Date:1 March 2024</h5>
+                </div>
+                <div className='description'>
+                    <p>"Avanta" is a vibrant annual college-level event that serves as a melting pot of technical innovation and creative expression, attracting participation from diverse engineering departments. Combining both technical and non-technical competitions, Avanta offers a platform for students to showcase their skills, ideas, and talents in a competitive yet supportive environment.</p>
+                </div>
+            </div>
+            <div className='content2'>
+                
+                <div className='description'>
+                    <p>"Avanta" is a vibrant annual college-level event that serves as a melting pot of technical innovation and creative expression, attracting participation from diverse engineering departments. Combining both technical and non-technical competitions, Avanta offers a platform for students to showcase their skills, ideas, and talents in a competitive yet supportive environment.</p>
+                </div>
+                <div className='container'>
+                <h2>AVANTA FEST</h2>
+                <h4>Mode:Offline/Online</h4>
+                <h4>Sri Krishna College of Technology,Coimbatore</h4>
+                <h5>Start Date:1 March 2024</h5>
+                <h5>End Date:1 March 2024</h5>
+                </div>
+                <div className='cont2'>
+              
+
+                   <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxT0TGuuOhC9RUHAHE6Krw9vMOtQSUryeLFhspIEGihg&s"/>
+               
+                <div className='details'>
+                <input type="submit" value="View details" className="auth-btn" />
+                </div>
+                </div>
+            </div>
+            <div className='content3'>
+            <div className='cont3'>
+              
+                   <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxT0TGuuOhC9RUHAHE6Krw9vMOtQSUryeLFhspIEGihg&s"/>
+
+             
+                <div className='details'>
+                <input type="submit" value="View details" className="auth-btn" />
+                </div>
+                </div>
+                <div className='container'>
+                <h2>AVANTA FEST</h2>
+                <h4>Mode:Offline/Online</h4>
+                <h4>Sri Krishna College of Technology,Coimbatore</h4>
+                <h5>Start Date:1 March 2024</h5>
+                <h5>End Date:1 March 2024</h5>
+                </div>
+                <div className='description'>
+                    <p>"Avanta" is a vibrant annual college-level event that serves as a melting pot of technical innovation and creative expression, attracting participation from diverse engineering departments. Combining both technical and non-technical competitions, Avanta offers a platform for students to showcase their skills, ideas, and talents in a competitive yet supportive environment.</p>
+                </div>
+            </div>
+            <div className='content4'>
+                <div className='description'>
+                    <p>"Avanta" is a vibrant annual college-level event that serves as a melting pot of technical innovation and creative expression, attracting participation from diverse engineering departments. Combining both technical and non-technical competitions, Avanta offers a platform for students to showcase their skills and talents in a competitive yet supportive environment.</p>
+                </div>
+                <div className='container'>
+                <h2>AVANTA FEST</h2>
+                <h4>Mode:Offline/Online</h4>
+                <h4>Sri Krishna College of Technology,Coimbatore</h4>
+                <h5>Start Date:1 March 2024</h5>
+                <h5>End Date:1 March 2024</h5>
+                </div>
+            <div className='cont4'>
+             
+                   <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxT0TGuuOhC9RUHAHE6Krw9vMOtQSUryeLFhspIEGihg&s"/>
+
+                
+                <div className='details'>
+                <input type="submit" value="View details" className="auth-btn" />
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </>
+  );
+}
+
+export default Page;
+
